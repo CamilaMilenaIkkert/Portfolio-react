34924fcb46655f97044006ba06e33b675be5c99f
Camila Ikkert <camilaikkert@gmail.com>
2024-12-28 03:45:46 -0300

    Remove dist from version control

---
+++ src/app/dist/page.js
@@ -0,0 +1,9 @@
+"use client";
+"use strict";
+exports.__esModule = true;
+var page_1 = require("@/app/sobre-mi/page");
+function Home() {
+    return (React.createElement(React.Fragment, null,
+        React.createElement(page_1["default"], null)));
+}
+exports["default"] = Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 "use client"
-import Contacto from "@/app/contacto/page";
-import Proyectos from "@/app/proyectos/page";
 import SobreMi from "@/app/sobre-mi/page";
 
 export default function Home() {
